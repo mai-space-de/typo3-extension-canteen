3dab4e015de084c6ebe458927650aef7a6516645
maispace <joel@maispace.de>
2026-04-11 18:48:12 +0200

    feat: initial scaffold for mai_canteen

---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Mai Canteen',
+    'description' => 'Canteen meal plan records with daily menus, dietary flags, and weekly scheduling.',
+    'category' => 'module',
+    'author' => 'Maispace',
+    'author_email' => '',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-14.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
