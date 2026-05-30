45dc3cefb409fac71f1fc876d62998ea0ce5256e
maispace <joel@maispace.de>
2026-05-30 14:04:40 +0200

    [BUGFIX] Add Extbase table name mappings for Canteen models
    Add Configuration/Extbase/Persistence/Classes.php to map Extbase
    models to short table names (tx_maicanteen_menuplan, tx_maicanteen_dish)
    instead of default domain_model naming convention.
    
    Prevents SqlErrorException when using Extbase repositories.

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \Maispace\MaiCanteen\Domain\Model\MenuPlan::class => [
+        'tableName' => 'tx_maicanteen_menuplan',
+    ],
+    \Maispace\MaiCanteen\Domain\Model\Dish::class => [
+        'tableName' => 'tx_maicanteen_dish',
+    ],
+];
